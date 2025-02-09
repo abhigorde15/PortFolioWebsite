accfe78e199700920e7de14538d37101dcd64ac4
abhigorde15 <166307032+abhigorde15@users.noreply.github.com>
2025-02-09 14:37:54 +0530

    Added New Project :- Smart Contact Manager

--- src/components/WorkCard.jsx
+++ src/components/WorkCard.jsx
@@ -10,7 +10,7 @@ const WorkCard =(props)=>{
             <p>{props.text}</p>
             <div className="pro-btns">
               <Link to ={props.view} className="btn" >VIEW</Link>
-              <Link to ="url.com" className="btn" >Souree</Link>
+              <Link to ="url.com" className="btn" >Source</Link>
             </div>
           </div>
       </div>
--- src/components/WorkCardData.jsx
+++ src/components/WorkCardData.jsx
@@ -8,7 +8,7 @@ const ProjectCardData =[
     igmsrc : project1,
     title :"Smart Contact Manager",
     text :"Developed a full-stack contact management system using Spring Boot for backend and MySQL as the Database ",
-    view :"https://github.com/abhigorde15",
+    view :"https://github.com/abhigorde15/SmartContactManager",
   },
   {
     igmsrc : project2,
@@ -19,8 +19,14 @@ const ProjectCardData =[
   {
     igmsrc : project3,
     title :"Todo Management Website",
-    text :"Developed a full-stack Todo management Website using Spring Boot for backend and React for Frontend",
+    text :"Developed a full-stack Todo management Website using Spring Boot & React for Frontend ",
     view :"https://github.com/abhigorde15/TodoProjectVersion2",
+  },
+  {
+    igmsrc : project3,
+    title :"Employee Management System",
+    text :"Developed a full-stack Todo management Website using Spring Boot & React.js",
+    view :"https://github.com/abhigorde15/EmployeeManagementSystem",
   }
   
 ]
